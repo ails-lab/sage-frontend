fc71283dd8380f6d1db34124019c0f6116dc30ad
Panagiotis Tzortzis <tzortzispanagiotis@gmail.com>
2024-07-01 14:10:48 +0300

    rm

--- src/js/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL;
-export const WS_BASE_URL = process.env.REACT_APP_WS_URL;
-export const ACCESS_TOKEN = 'accessToken';
-
-export const TIME_OPTIONS = {  year: 'numeric', month: 'numeric', day: 'numeric', hour : 'numeric', minute : 'numeric', second : 'numeric' };
